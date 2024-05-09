9a2f07571839f457c95d67caa46170ad17b232a0
unknown <javi@medac.com>
2024-05-09 22:15:52 +0200

    pito

---
+++ BACK2ENJOW-main/BACKEND-BACK2ENJOY/APIREST_BACK2ENJOY/database/migrations/2024_03_31_154905_create_valoraciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('valoraciones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('puntuacion');
+            $table->string('comentario');
+            $table->timestamps();
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('valoraciones');
+    }
+};
